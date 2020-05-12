8c27f8804df5d42bb76d5847cebc484701758222
zharan96prog <o.zharan96@gmail.com>
2020-05-12 17:24:30 +0300

    Tracking work

--- WpfClientNovaPoshta/Tracking.xaml.cs
+++ WpfClientNovaPoshta/Tracking.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -36,11 +37,23 @@ namespace WpfClientNovaPoshta
         private async Task<List<Datum>> GetAllDatum()
         {
             List<Datum> list = new List<Datum>();
-            using (HttpClient client = new HttpClient())
+            HttpWebRequest client = (HttpWebRequest)WebRequest.Create("http://api.novaposhta.ua/v2.0/json/");
             {
-                var response = await client.GetAsync("http://testapi.novaposhta.ua/v2.0/en/documentsTracking/json/");
-                string json = await response.Content.ReadAsStringAsync();
-                list = JsonConvert.DeserializeObject<List<Datum>>(json);
+                client.Method = "POST";
+                client.ContentType = "application/json";
+
+                var streamOut = new StreamWriter(client.GetRequestStream());
+                string api = "fa65506491a98ed202663e43d0144f3b";
+                string json = String.Format("{\"apiKey\":\"{0}\",\"modelName\":\"TrackingDocument\",\"calledMethod\":\"getStatusDocuments\",\"methodProperties\":{\"Documents\":[{\"DocumentNumber\":\"20450234771943\",\"Phone\":\"0954165091\"}]}}", api);
+                streamOut.Write(json);
+                streamOut.Close();
+                var responce = client.GetResponse();
+                var stream = new StreamReader(responce.GetResponseStream());
+                var text = stream.ReadToEnd();
+                lbl.Content = text;
+
+              //  string json = await response.Content.ReadAsStringAsync();
+              //  list = JsonConvert.DeserializeObject<List<Datum>>(json);
             }
             return list;
         }
