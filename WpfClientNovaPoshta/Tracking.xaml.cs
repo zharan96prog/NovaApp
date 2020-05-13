b390c83c1417ad90f5f9c23101ef9de6ba3d5ba0
zharan96prog <o.zharan96@gmail.com>
2020-05-13 14:48:41 +0300

    DaateOfDelivery

--- WpfClientNovaPoshta/Tracking.xaml.cs
+++ WpfClientNovaPoshta/Tracking.xaml.cs
@@ -29,33 +29,26 @@ namespace WpfClientNovaPoshta
             InitializeComponent();
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            await GetAllDatum();
-        }
-
-        private async Task<List<Datum>> GetAllDatum()
-        {
-            List<Datum> list = new List<Datum>();
             HttpWebRequest client = (HttpWebRequest)WebRequest.Create("http://api.novaposhta.ua/v2.0/json/");
             {
                 client.Method = "POST";
                 client.ContentType = "application/json";
 
                 var streamOut = new StreamWriter(client.GetRequestStream());
-                string api = "fa65506491a98ed202663e43d0144f3b";
-                string json = String.Format("{\"apiKey\":\"{0}\",\"modelName\":\"TrackingDocument\",\"calledMethod\":\"getStatusDocuments\",\"methodProperties\":{\"Documents\":[{\"DocumentNumber\":\"20450234771943\",\"Phone\":\"0954165091\"}]}}", api);
+                //string api = "fa65506491a98ed202663e43d0144f3b";
+                string json = "{\"apiKey\":\"fa65506491a98ed202663e43d0144f3b\",\"modelName\":\"TrackingDocument\",\"calledMethod\":\"getStatusDocuments\",\"methodProperties\":{\"Documents\":[{\"DocumentNumber\":\"20450234771943\",\"Phone\":\"0954165091\"}]}}";
                 streamOut.Write(json);
                 streamOut.Close();
                 var responce = client.GetResponse();
                 var stream = new StreamReader(responce.GetResponseStream());
                 var text = stream.ReadToEnd();
-                lbl.Content = text;
-
-              //  string json = await response.Content.ReadAsStringAsync();
-              //  list = JsonConvert.DeserializeObject<List<Datum>>(json);
+                //lbl.Content = text;
+                Datum datum = JsonConvert.DeserializeObject<Datum>(text);
+                lbl1.Content = datum.RefCitySender;
             }
-            return list;
         }
+
     }
 }
