c0b9a92d111b360c6f1b948eda4ea3f335cc6517
zharan96prog <o.zharan96@gmail.com>
2020-05-14 14:14:06 +0300

    need fix bags

--- WpfClientNovaPoshta/Model/CostInfo.cs
+++ WpfClientNovaPoshta/Model/CostInfo.cs
@@ -6,25 +6,33 @@ using System.Threading.Tasks;
 
 namespace WpfClientNovaPoshta.Model
 {
+
+    public class RootobjectCost
+    {
+        public bool success { get; set; }
+        public DatumCost[] data { get; set; }
+        public object[] errors { get; set; }
+        public string[] warnings { get; set; }
+        public object[] info { get; set; }
+        public object[] messageCodes { get; set; }
+        public object[] errorCodes { get; set; }
+        public string[] warningCodes { get; set; }
+        public object[] infoCodes { get; set; }
+    }
+
     public class DatumCost
     {
-        public int CostRedelivery { get; set; }
         public int AssessedCost { get; set; }
         public int Cost { get; set; }
+        public int CostRedelivery { get; set; }
+        public Tzoneinfo TZoneInfo { get; set; }
         public int CostPack { get; set; }
     }
 
-    public class CostInfo
+    public class Tzoneinfo
     {
-        public bool success { get; set; }
-        public IList<Datum> data { get; set; }
-        public IList<object> errors { get; set; }
-        public IList<string> warnings { get; set; }
-        public IList<object> info { get; set; }
-        public IList<object> messageCodes { get; set; }
-        public IList<object> errorCodes { get; set; }
-        public IList<string> warningCodes { get; set; }
-        public IList<object> infoCodes { get; set; }
+        public string TzoneName { get; set; }
+        public string TzoneID { get; set; }
     }
 
 }
--- WpfClientNovaPoshta/View/CostInfo.xaml.cs
+++ WpfClientNovaPoshta/View/CostInfo.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +14,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WpfClientNovaPoshta.Model;
+using WpfClientNovaPoshta.ViewModel.Helpers;
 
 namespace WpfClientNovaPoshta
 {
@@ -19,9 +24,44 @@ namespace WpfClientNovaPoshta
     /// </summary>
     public partial class CostInfo : Window
     {
+        public List<DatumCity> citySenderr = new List<DatumCity>();
+        public List<DatumCity> cityReiceptt = new List<DatumCity>();
         public CostInfo()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            HttpWebRequest client = (HttpWebRequest)WebRequest.Create("http://api.novaposhta.ua/v2.0/json/");
+            {
+                client.Method = "POST";
+                client.ContentType = "application/json";
+                var streamOut = new StreamWriter(client.GetRequestStream());
+                string api = "fa65506491a98ed202663e43d0144f3b";
+                string citySender = citySenderr[0].Addresses[cmbCitySender.SelectedIndex].Ref;
+                var cityRecipient = cityReiceptt[0].Addresses[cmbCityreicept.SelectedIndex].Ref;
+                string json = "{\"modelName\": \"InternetDocumen\", \"calledMethod\": \"getDocumentPrice\", \"methodProperties\": {\"CitySender\": " + $"\" {citySender}" + "\",\"CityRecipient\":  " + $"\" {cityRecipient}" + "\",\"Weight\":  " + $"\" {weigth.Text}" + "\",\"ServiceType\": \"DoorsDoors\",\"Cost\":  " + $"\" {cost.Text}" + "\", \"CargoType\": \"Cargo\",\"SeatsAmount\":\"1\",\"PackCalculate\": {\"PackCount\": \"1\",\"PackRef\": \"1499fa4a-d26e-11e1-95e4-0026b97ed48a\"},\"RedeliveryCalculate\": {\"CargoType\": \"Money\",\"Amount\": " + $"\" {amount.Text}" + "\"} },\"apiKey\": " + $"\" {api}" + "\"}";
+                streamOut.Write(json);
+                streamOut.Close();
+                var responce = client.GetResponse();
+                var stream = new StreamReader(responce.GetResponseStream());
+                var text = stream.ReadToEnd();
+                RootobjectCost datum = JsonConvert.DeserializeObject<RootobjectCost>(text);
+                lblCost.Content = datum.data[0].Cost;
+            }
+        }
+
+        private async void citySender_LostFocus(object sender, RoutedEventArgs e)
+        {
+            citySenderr = await NovaPoshtaAppHelper.GetCities(citySender.Text);
+            cmbCitySender.ItemsSource = citySenderr[0].Addresses;
+        }
+
+        private async void cityReicept_LostFocus(object sender, RoutedEventArgs e)
+        {
+            cityReiceptt = await NovaPoshtaAppHelper.GetCities(cityReicept.Text);
+            cmbCityreicept.ItemsSource = cityReiceptt[0].Addresses;
         }
     }
 }
